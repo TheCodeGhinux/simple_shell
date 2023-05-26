db0b0aaedf2dd942266c02199b4defe62b9b24c8
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-05-26 12:14:53 +0100

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -109,51 +109,6 @@ int shell_exit(char **args, char **cmdline)
 }
 
 /**
- * shell_exit - Causes normal process termination
- *                for the shell shell.
- * @args: An array of arguments containing the exit value.
- * @cmdline: A double pointer to the beginning of args.
- *
- * Return: -3 If there are no arguments.
- *         -2 If the given exit value is invalid.
- *         O/w - exits with the given status value.
- *
- * Description: Upon returning -3, the program exits back in the main function.
- */
-int shell_exit(char **args, char **cmdline)
-{
-	int i, len_of_int = 10;
-	unsigned int num = 0, max = 1 << (sizeof(int) * 8 - 1);
-
-	if (args[0])
-	{
-		if (args[0][0] == '+')
-		{
-			i = 1;
-			len_of_int++;
-		}
-		for (; args[0][i]; i++)
-		{
-			if (i <= len_of_int && args[0][i] >= '0' && args[0][i] <= '9')
-				num = (num * 10) + (args[0][i] - '0');
-			else
-				return (create_error(--args, 2));
-		}
-	}
-	else
-	{
-		return (-3);
-	}
-	if (num > max - 1)
-		return (create_error(--args, 2));
-	args -= 1;
-	free_args(args, cmdline);
-	free_env();
-	free_alias_list(aliases);
-	exit(num);
-}
-
-/**
  * shell_cd - Changes the current directory of the shell process.
  * @args: An array of arguments.
  * @cmdline: A double pointer to the beginning of args.
